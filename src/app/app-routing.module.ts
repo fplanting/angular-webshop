f4b83666677613a2c804af109ffa6877762c0980
fplanting <frida.planting@hotmail.com>
2022-02-26 15:31:52 +0100

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{ path: '', component: HomeComponent },
+{ path: 'movies', component: MovieListComponent },
+{ path: 'movie', component: MovieDetailsComponent },
+{ path: 'checkout', component: CheckoutComponent },
+{ path: '**', component: NotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
+    NotfoundComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
