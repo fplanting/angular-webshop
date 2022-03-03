ac440bc9341383c9459af304948ac5f85e0654ca
fplanting <frida.planting@hotmail.com>
2022-03-03 13:30:10 +0100

    added interface for orderRow

--- src/app/models/IOrder.ts
+++ src/app/models/IOrder.ts
@@ -1,6 +1,10 @@
+import { IOrderRow } from "./IOrderRow";
+
 export interface IOrder {
     id: number;
     companyId: number;
     created: string;
     totalPrice: number;
+    status: number;
+    orderRows: IOrderRow[];
 }
