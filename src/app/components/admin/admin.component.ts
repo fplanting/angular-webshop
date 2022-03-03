0dd3d8dcb293f94ba5e15b646217042fd2412ef5
fplanting <frida.planting@hotmail.com>
2022-03-03 16:56:29 +0100

    fixed sending orders to api and a button on checkout

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/models/IOrder';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  orders: IOrder[] = [];
 
-  constructor() { }
+  constructor(private orderservice: OrderService) { }
 
   ngOnInit(): void {
+    this.orderservice.getOrders().subscribe(data => {
+      this.orders = data;
+    });
   }
 
+  removeOrder(order: any) {
+    this.orderservice.deleteOrder(order.id).subscribe();
+    for (let i = 0; i < this.orders.length; i++) {
+      if (order.id === this.orders[i].id) {
+        this.orders.splice(i, 1);
+      }
+    }
+  }
 }
