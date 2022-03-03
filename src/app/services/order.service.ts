d33d9cde45398fcef0f368ffa81fe83a1ee6cd50
fplanting <frida.planting@hotmail.com>
2022-03-03 13:22:12 +0100

    added functions for posting orders to api

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IOrder } from '../models/IOrder';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private http: HttpClient) { }
+
+  getOrders(): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(environment.apiUrl + '/orders?companyId=37');
+  }
+
+  deleteOrder(id: number): Observable<IOrder> {
+    return this.http.delete<IOrder>(environment.apiUrl + '/orders/' + id);
+  }
+
+  postOrder(order: IOrder) {
+    return this.http.post(environment.apiUrl + '/orders/', order);
+  }
+
+}
